6761442a2c13f9d38c15c2205425bc0682bc8563
thebiggestblackestn3ju <nassim.sidllj@gmail.com>
2024-12-27 04:47:52 -0500

    AutoClicker.java

--- src/com/ruffian7/sevenclicker/AutoClicker.java
+++ src/com/ruffian7/sevenclicker/AutoClicker.java
@@ -30,8 +30,8 @@ public class AutoClicker {
 
 	private static int delay = -1;
 	public static long lastTime = 0;
-	public static int minCPS = 8;
-	public static int maxCPS = 12;
+	public static int minCPS = 800;
+	public static int maxCPS = 1200;
 	public static int button = 1;
 
 	public static String[] toggleKey = { "", "" };
